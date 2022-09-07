911de4dba177a7f5294c109ebfdd20b8c59da6c1
NicolleCosta <nicolle.scosta@outlook.com>
2022-09-07 20:55:39 -0300

    Clean

--- beibe/src/java/com/ufpr/tads/beibe/dao/EstadoDAO.java
+++ beibe/src/java/com/ufpr/tads/beibe/dao/EstadoDAO.java
@@ -23,34 +23,8 @@ public class EstadoDAO {
     private static final String QUERY_BUSCAR = "select id, nome, sigla from estado";
     
     private Connection con= null;
-    
-   /* public List<Estado> buscarTodos(){
-        Connection con = null;
-        PreparedStatement st = null;
-        ResultSet rs = null;
-        List<Estado> estados = new ArrayList<Estado>();
-        
-        try{
-            Class.forName(com.ufpr.tads.beibe.dao.ConnectionFactory.DRIVER);
-            con = DriverManager.getConnection(com.ufpr.tads.beibe.dao.ConnectionFactory.URL, 
-                                                com.ufpr.tads.beibe.dao.ConnectionFactory.LOGIN, 
-                                                com.ufpr.tads.beibe.dao.ConnectionFactory.SENHA);
-        st = con.prepareStatement(QUERY_BUSCAR);
-        rs = st.executeQuery();    
-            while(rs.next()){
-                    Estado estado = new Estado();
-                    estado.setId(rs.getInt(1));
-                    estado.setNome(rs.getString(2));
-                    estado.setSigla(rs.getString(3));  
-                    estados.add(estado);
-                    
-                }  
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return  estados;
-    }*/
-    
+  
+   
     public static List<Estado> buscarTudo() {
         Connection con = null;
         PreparedStatement st = null;
